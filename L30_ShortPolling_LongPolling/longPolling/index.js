a00d47fcc31f2096585c363f60891348b8347876
Pratiyush Ray <pkray0021@gmail.com>
2025-09-11 10:59:46 +0530

    short Polling & long Polling

---
+++ L30_ShortPolling_LongPolling/longPolling/index.js
@@ -0,0 +1,36 @@
+
+
+const express = require("express");
+const app = express();
+
+let name = "Aakash (Intial Data)"
+const pendingReq = [];
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.get("/getname",(req,res)=>{
+    if (name !== req.query.lastName){
+        res.json({name});
+    }else{
+        pendingReq.push(res);
+    }
+})
+
+
+app.get("/updatename",(req,res)=>{
+    name = req.query.name;
+
+    while( pendingReq.length> 0){
+        const client = pendingReq.pop();
+        client.json({name});
+
+    }
+})
+
+
+const port = process.env.PORT || 5002;
+
+app.listen(port, ()=>{
+    console.log(`Server Running on Port ${port}`)
+})
